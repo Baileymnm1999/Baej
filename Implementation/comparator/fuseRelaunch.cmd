f75e90fd2305b579e0cda1e5aaca19a0f716b16b
dripchar <dripchar@gmail.com>
2019-01-22 23:39:19 -0500

    My worklog and comparator

---
+++ Implementation/comparator/fuseRelaunch.cmd
@@ -0,0 +1,1 @@
+-intstyle "ise" -incremental -lib "unisims_ver" -lib "unimacro_ver" -lib "xilinxcorelib_ver" -o "C:/Users/dripchar/Documents/Classes/CSSE232/comparator/comp_tb_isim_beh.exe" -prj "C:/Users/dripchar/Documents/Classes/CSSE232/comparator/comp_tb_beh.prj" "work.comp_tb" "work.glbl" 
