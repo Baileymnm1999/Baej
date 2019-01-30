93ded61d0444984d397e3fbeb6edfee380a27cde
dripchar <dripchar@gmail.com>
2019-01-30 18:39:00 -0500

    added the final regfile

---
+++ Implementation/regfile16b63/fuseRelaunch.cmd
@@ -0,0 +1,1 @@
+-intstyle "ise" -incremental -lib "unisims_ver" -lib "unimacro_ver" -lib "xilinxcorelib_ver" -o "C:/Users/dripchar/Documents/Classes/CSSE232/regfile16b63/regfile16b64_tb_0_isim_beh.exe" -prj "C:/Users/dripchar/Documents/Classes/CSSE232/regfile16b63/regfile16b64_tb_0_beh.prj" "work.regfile16b64_tb_0" "work.glbl" 
