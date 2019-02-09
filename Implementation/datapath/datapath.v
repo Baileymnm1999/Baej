e10fd55c9fdddaec23803ca00e2b0729f99fe935
Bailey Morgan <bailey.mccarty.morgan@gmail.com>
2019-02-09 00:11:44 -0500

    Started datapath integration

---
+++ Implementation/datapath/datapath.v
@@ -0,0 +1,27 @@
+`timescale 1ns / 1ps
+//////////////////////////////////////////////////////////////////////////////////
+// Company: 
+// Engineer: 
+// 
+// Create Date:    00:09:27 02/09/2019 
+// Design Name: 
+// Module Name:    datapath 
+// Project Name: 
+// Target Devices: 
+// Tool versions: 
+// Description: 
+//
+// Dependencies: 
+//
+// Revision: 
+// Revision 0.01 - File Created
+// Additional Comments: 
+//
+//////////////////////////////////////////////////////////////////////////////////
+module datapath(
+    input [15:0] ioIn,
+    output [16:0] ioOut
+    );
+
+
+endmodule
